a9bac3bf1f79b3530ac06929d20b9d6767d8599d
izaz <shekhizaz@gmail.com>
2021-02-15 09:32:47 +0000

    initialAll

---
+++ FrontEnd/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-property-card',
+  //template: `<h1>I am a Card</h1>`,
+  templateUrl: 'property-card.component.html',
+  //styles:['h1 {font-weight: normal;}']
+  styleUrls: ['property-card.component.css']
+})
+export class PropertyCardComponent {
+  Property: any={
+    "Id":1,
+    "name":"Dale Street",
+    "Type":"House",
+    "Price":12000
+  }
+}
