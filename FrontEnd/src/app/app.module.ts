7da6d99e0b0ccaeb705996ab7012f94f29bf33c1
izaz <shekhizaz@gmail.com>
2021-02-19 19:35:30 +0000

    LoginRegisterComponentAdded

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -10,12 +10,17 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { DwellingService } from './services/dwelling.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
+import { FormsModule } from '@angular/forms';
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
 
 const appRoutes:Routes=[
   {path: '', component: PropertyListComponent},
   {path: 'rent-property', component: PropertyListComponent},
   {path: 'add-property', component: AddPropertyComponent},
   {path: 'property-detail/:id', component: PropertyDetailComponent},
+  {path: 'user/login', component: UserLoginComponent},
+  {path: 'user/register', component: UserRegisterComponent},
   {path: '**', component: PropertyListComponent}//routing for page not found error
 ]
 
@@ -26,12 +31,15 @@ const appRoutes:Routes=[
     PropertyListComponent,
     NavBarComponent,
     AddPropertyComponent,
-    PropertyDetailComponent
+    PropertyDetailComponent,
+    UserLoginComponent,
+    UserRegisterComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [
     DwellingService
--- FrontEnd/src/app/property/add-property/add-property.component.ts
+++ FrontEnd/src/app/property/add-property/add-property.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,13 +8,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./add-property.component.css']
 })
 export class AddPropertyComponent implements OnInit {
-
+  @ViewChild('Form') addPropertyForm !: NgForm;
   constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
   onBack() {
     this.router.navigate(['/']);
+  }
+
+  onSubmit() {
+    console.log("Submitted form");
+    console.log(this.addPropertyForm);
   }
 
 }
