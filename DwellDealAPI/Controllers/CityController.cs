5572999c4127151f2ab1f63b3af4d930473925e1
izaz <shekhizaz@gmail.com>
2021-03-11 12:02:17 +0000

    .netapiadded

---
+++ DwellDealAPI/Controllers/CityController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using DwellDealAPI.Models;
+
+namespace DwellDealAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        public CityController()
+        {
+        }
+
+        [HttpGet("")]
+        public IEnumerable<string> Getstrings()
+        {
+            return new string[] { "ghhjjj","London","Leiceter","Manchester"};
+        }
+    }
+}
